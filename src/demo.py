13fe1c83d9a7f90e916ef691c9425211e3a8850a
Babis Babalis <babisbabalis@gmail.com>
2021-02-25 22:28:44 +0200

    nodes updated with supermarkets

--- src/demo.py
+++ src/demo.py
@@ -5,6 +5,13 @@
 # imports here
 import pandas as pd
 import json
+import osmnx as ox
+import numpy as np
+import matplotlib.pyplot as plt
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..', 'osm_project/src')))
+import network_operations as net_ops
+import network_scenarios as net_scens
 import pdb
 
 # methods here
@@ -23,12 +30,12 @@ def create_pois_df(POIs_fpath):
     for coord in pois_list:
         coords = coord['geometry']['coordinates']
         coords_list.append(coords)
-    points_list = []
-    get_single_point_from_coords_list(coords_list, points_list)
-    return points_list
+    point_coords_list = []
+    get_single_point_from_coords_list(coords_list, point_coords_list)
+    return point_coords_list
 
 
-def get_single_point_from_coords_list(coords_list, points_list, polygon=False):
+def get_single_point_from_coords_list(coords_list, point_coords_list, polygon=False):
     """Method to examine a list of points/polygons coords and to return
     a single point for each point/polygon.
     
@@ -48,32 +55,61 @@ def get_single_point_from_coords_list(coords_list, points_list, polygon=False):
     # break is necessary for the recursion to work.
     for coord in coords_list:
         if len(coord) == 2:
-            points_list.append(coord)
+            point_coords_list.append(tuple(coord))
             if polygon == True:
                 break
         else:
-            get_single_point_from_coords_list(coord, points_list, polygon=True)
+            get_single_point_from_coords_list(coord, point_coords_list, polygon=True)
 
 
-def assign_points_to_nodes(points_list, nodes):
+def assign_points_to_nodes(point_coords_list, graph):
     """Method to assign points of interest to nodes.
     In order to assign points to nodes the coordinates are used.
     For each point the node nearest to it (according to the coordinates) is found.
 
     Args:
-        points_list (list): List of coordinates (points coordinates)
-        nodes (Dataframe): dataframe of nodes.
+        point_coords_list (list): List of coordinates (points coordinates)
+        graph (graph object): graph representation of the network.
     """
-    pass
+    del point_coords_list[0]
+    X, Y = zip(*point_coords_list)
+    nearest_node_id_list = ox.distance.get_nearest_nodes(graph, X, Y)
+    return nearest_node_id_list.tolist()
+
+
+def update_graph_nodes_with_POIs(nodes, assigned_nodes_list, new_col='supermarket'):
+    """Method to update the nodes df with new fields and nodes.
+
+    Args:
+        nodes (dataframe): pandas dataframe that represents the nodes of
+        the network.
+        assigned_nodes_list (list): list of network nodes nearest to the
+        points of interest.
+    """
+    nodes[new_col] = False
+    for node in assigned_nodes_list:
+        nodes.loc[nodes['osmid'] == node, new_col] = True
+
+
+def plot_graph_and_points(graph, points_list):
+    fig, ax = ox.plot_graph(graph, show=False, close=False)
+    for point_coords in points_list:
+        point_a, point_b = point_coords
+        ax.scatter(point_a, point_b, c='red')
+        plt.show()
 
 
 def main():
     # declare all variables (may be input vars later)
-    POIs_fpath = 'data/butchershops-attiki.geojson'
-    graph_fpath = ''
+    POIs_fpath = 'data/attica-supermarket.geojson'
+    graph_fpath = 'data/attica_graph.graphml'
     algorithm = ''
     # read points of interest (data)
-    create_pois_df(POIs_fpath)
+    point_coords_list = create_pois_df(POIs_fpath)
+    graph = net_ops.load_graph_from_disk(graph_fpath)
+    assigned_nodes_list = assign_points_to_nodes(point_coords_list, graph)
+    nodes, edges = net_ops.get_nodes_edges(graph)
+    update_graph_nodes_with_POIs(nodes, assigned_nodes_list)
     pdb.set_trace()
     # read/download network of interest
     # merge network to point of interest
